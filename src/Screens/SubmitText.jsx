351dc5a6bd44291d921aae81c4a4664cf7d554cb
Vitor Ribeiro <vitor.camposrdc@gmail.com>
2023-04-18 22:24:47 -0300

    working in post method

--- src/Screens/SubmitText.jsx
+++ src/Screens/SubmitText.jsx
@@ -1,6 +1,32 @@
+import { useState } from 'react'
 import { Header } from '../Components/Header'
+import { useAuth } from '../Context/AuthProvider/useAuth'
+import { api } from '../services/api'
 
 export function SubmitText() {
+  const [text, setText] = useState('')
+  const [success, setSuccess] = useState(false)
+  const [data, setData] = useState({ id: '', content: '' })
+
+  const auth = useAuth()
+
+  console.log(auth)
+
+  async function handlePostData() {
+    setData({
+      id: '1',
+      content: text,
+    })
+    try {
+      await api.post(`/alunos/redacao/create`, data, {
+        headers: { Authorization: `Bearer ${auth.user.token}` },
+      })
+      setSuccess(true)
+    } catch (error) {
+      return alert(error)
+    }
+  }
+
   return (
     <div className="w-screen h-screen flex-1 justify-center items-center">
       <Header />
@@ -10,10 +36,15 @@ export function SubmitText() {
           type="text"
           placeholder="Digite aqui seu texto"
           className="w-full h-48 text-center rounded-lg bg-blue-200 border border-blue-400"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
         />
       </div>
       <div className="w-full flex items-center justify-center">
-        <button className="bg-green-500 w-32 text-white p-2 rounded-lg">
+        <button
+          className="bg-green-500 w-32 text-white p-2 rounded-lg"
+          onClick={handlePostData}
+        >
           Enviar
         </button>
       </div>
